d603c409f686ba86def444f3f83a2901d9504ee6
NejdetSonmez <46686507+NejdetSonmez@users.noreply.github.com>
2019-11-11 17:52:51 +0300

    Add files via upload

---
+++ SumOfMultiple/SumOfMultiple/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfMultiple
+{
+    class Program
+    {
+        static void SumOfMultiple(int limit)
+        {          
+            int sum = 0;
+            for (int i = 1; i < limit; i++)
+            {
+                int value2 = i % 3 == 0 ? 3 : 0;
+                int value1 = i % 5 == 0 ? 5 : 0;
+                sum += (value1 + value2) > 0 ? i : 0;
+            }
+            OutPut(sum);
+        }
+        static void OutPut(int summation)
+        {
+          Console.WriteLine("Output: {0}",summation);
+        }
+        static void Main(string[] args)
+        {
+            int input;
+            Console.WriteLine("Welcome to SumOfMultiple! Please, enter a limit.");
+            Console.Write("Input:");      
+            input = Convert.ToInt16(Console.ReadLine());
+            SumOfMultiple(input);
+            Console.ReadKey();      
+        }
+    }
+}
